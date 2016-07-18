28a300749e82663c3018768dde5233e4c727f7f9
Yasmin Feitosa <yasmin.feitosa@nimaltecnologia.corp>
2016-07-18 17:59:10 -0300

    - ALaSQL adicionado na pasta node_modules. - As pastas do ALaSQL, xlsx e xlsjs foram tiradas do node_modules e colocadas na plugins.
    - Vários links relativos ao ALaSQL foram adicionados no index.html e comentados.
    
    - Tentando implentar um controller que leia o xlsx (com alaSQL) (Não funcionando / Problemas em encontrar o arquivo?)
    
    - Plugins ui-Grid e ngTouch foram comentados.
    
    - Angular atualizado (antigo está na pasta angular_old)
    
    - Links úteis no controller de listaLocaisCtrl
    
    ps: ainda não sei o que estou fazendo. ._.
    -----------------------------------------------------------------------------------------------------------------------------------------------------------
    
    1 – Carregar em memória os dois arquivos (Lista de Bens.xlsx e Lista de locais.xlsx) (estão dentro da pasta)
    2 – Ao iniciar o aplicativo, solicitar ao usuário o local em que ele está (Lista de Locais.xlss). Apresentar na tela o COD_LOCAL e o DESC_LOCAL.
    3 – Ao ler o código de barras do bem (CHAPA), apresentar na tela a DESC_BEM e validar o COD_LOCAL do arquivo (Lista de Bens.xlsx) contra o local informado no Passo 1.
    4 – Caso o cod do local esteja diferente, dar a opção ao usuário de trocá-lo: O COD_LOCAL informado diverge do cadastro. Deseja atualizar?

--- App/www/js/controller/listaLocaisCtrl.js
+++ App/www/js/controller/listaLocaisCtrl.js
@@ -14,6 +14,7 @@ angular.module('starter').controller('listaLocaisCtrl', function($scope, $state,
 // http://codetheory.in/parse-read-excel-files-xls-xlsx-javascript/
 
 
+//**********************************************************************************************************//
 
 // USANDO UI-GRID
 // http://ui-grid.info/docs/#/tutorial/101_intro
@@ -40,6 +41,8 @@ angular.module('starter').controller('listaLocaisCtrl', function($scope, $state,
 //     }
 // ];
 
+
+//**********************************************************************************************************//
 
 
 // function loadJSON(callback) {
@@ -70,27 +73,202 @@ angular.module('starter').controller('listaLocaisCtrl', function($scope, $state,
 
 
 
+//**********************************************************************************************************//
+
+
+
+//// Fazendo este aqui >>>>>>>>>>>>>>>>>  http://ui-grid.info/docs/#/tutorial/201_editable
+
+
+
+// var myString = '{"COD_BEM":0000000001,"DESC_BEM":"SISTEMA OPERACIONAL WINDOWS XP PRO SP3 C","CHAPA":0000000001C,"COD_LOCAL":000053},{"COD_BEM":0000000001,"DESC_BEM":"SOFTWARE CYBERLINK POWER DVD 8.3 SEM MID","CHAPA":0000000001B,"COD_LOCAL":000053}';
+// console.log(JSON.parse(myString));
+//
+//
+// $scope.gridOptions = {  };
+//
+// $scope.storeFile = function( gridRow, gridCol, files ) {
+//   // ignore all but the first file, it can only select one anyway
+//   // set the filename into this column
+//   gridRow.entity.filename = files[0].name;
+//
+//   // read the file and set it into a hidden column, which we may do stuff with later
+//   var setFile = function(fileContent){
+//     gridRow.entity.file = fileContent.currentTarget.result;
+//     // put it on scope so we can display it - you'd probably do something else with it
+//     $scope.lastFile = fileContent.currentTarget.result;
+//     $scope.$apply();
+//   };
+//   var reader = new FileReader();
+//   reader.onload = setFile;
+//   reader.readAsText( files[0] );
+// };
+//
+// $scope.gridOptions.columnDefs = [
+//   { name: 'COD_BEM', enableCellEdit: false, displayName: 'CÓDIGO', width: '15%' },
+//   { name: 'DESC_BEM', displayName: 'DESCRIÇÃO (editable)', width: '30%' },
+//   { name: 'CHAPA', enableCellEdit: false, width: '15%' },
+//   { name: 'COD_LOCAL', displayName: 'CÓDIGO LOCAL (editable)', width: '20%' }
+// ];
+//
+//
+//
+//
+//
+//
+//   // ///////////////// MDS, OLHA QUANTA COISA
+//
+//   // { name: 'age', displayName: 'Age' , type: 'number', width: '10%' },
+//   // { name: 'gender', displayName: 'Gender', editableCellTemplate: 'ui-grid/dropdownEditor', width: '20%',
+//   //   cellFilter: 'mapGender', editDropdownValueLabel: 'gender', editDropdownOptionsArray: [
+//   //   { id: 1, gender: 'male' },
+//   //   { id: 2, gender: 'female' }
+//   // ] },
+//   // { name: 'registered', displayName: 'Registered' , type: 'date', cellFilter: 'date:"yyyy-MM-dd"', width: '20%' },
+//   // { name: 'address', displayName: 'Address', type: 'object', cellFilter: 'address', width: '30%' },
+//   // { name: 'address.city', displayName: 'Address (even rows editable)', width: '20%',
+//   //      cellEditableCondition: function($scope){
+//   //      return $scope.rowRenderIndex%2;
+//   //      }
+//   // },
+//   // { name: 'isActive', displayName: 'Active', type: 'boolean', width: '10%' },
+//   // { name: 'pet', displayName: 'Pet', width: '20%', editableCellTemplate: 'ui-grid/dropdownEditor',
+//   //   editDropdownRowEntityOptionsArrayPath: 'foo.bar[0].options', editDropdownIdLabel: 'value'
+//   // },
+//   // { name: 'status', displayName: 'Status', width: '20%', editableCellTemplate: 'ui-grid/dropdownEditor',
+//   //   cellFilter: 'mapStatus',
+//   //   editDropdownOptionsFunction: function(rowEntity, colDef) {
+//   //     var single;
+//   //     var married = {id: 3, value: 'Married'};
+//   //     if (rowEntity.gender === 1) {
+//   //       single = {id: 1, value: 'Bachelor'};
+//   //       return [single, married];
+//   //     } else {
+//   //       single = {id: 2, value: 'Nubile'};
+//   //       return $timeout(function() {
+//   //         return [single, married];
+//   //       }, 100);
+//   //     }
+//   //   }
+//   // },
+//   // { name: 'filename', displayName: 'File', width: '20%', editableCellTemplate: 'ui-grid/fileChooserEditor',
+//   //   editFileChooserCallback: $scope.storeFile }
+//
+//
+// $scope.msg = {};
+//
+// $scope.gridOptions.onRegisterApi = function(gridApi){
+//         //set gridApi on scope
+//         $scope.gridApi = gridApi;
+//         gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
+//           $scope.msg.lastCellEdited = 'edited row id:' + rowEntity.id + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue ;
+//           $scope.$apply();
+//         });
+//       };
+//
+// $http.get ('js/bens.json')
+//   .success(function(data) {
+//     //for(i = 0; i < data.length; i++){
+//       //data[i].registered = new Date(data[i].registered);
+//       //data[i].gender = data[i].gender==='male' ? 1 : 2;
+//     //   if (i % 2) {
+//     //     data[i].pet = 'fish';
+//     //     data[i].foo = {bar: [{baz: 2, options: [{value: 'fish'}, {value: 'hamster'}]}]};
+//     //   }
+//     //   else {
+//     //     data[i].pet = 'dog';
+//     //     data[i].foo = {bar: [{baz: 2, options: [{value: 'dog'}, {value: 'cat'}]}]};
+//     //   }
+//     //}
+//
+//     $scope.gridOptions.data = data;
+//   });
+//
+//
+// // .filter('mapGender', function() {
+// // var genderHash = {
+// //   1: 'male',
+// //   2: 'female'
+// // };
+// //
+// // return function(input) {
+// //   if (!input){
+// //     return '';
+// //   } else {
+// //     return genderHash[input];
+// //   }
+// // };
+// // })
+// //
+// // .filter('mapStatus', function() {
+// // var genderHash = {
+// //   1: 'Bachelor',
+// //   2: 'Nubile',
+// //   3: 'Married'
+// // };
+// //
+// // return function(input) {
+// //   if (!input){
+// //     return '';
+// //   } else {
+// //     return genderHash[input];
+// //   }
+// // };
+//
+//
+//
+
+
+//**********************************************************************************************************//
+
+
+//// usando o  ALASQL
+//// https://github.com/agershun/alasql
+
+
+
+$scope.exportData = function () {
+        alasql('SELECT * INTO XLSX("john.xlsx",{headers:true}) FROM ?',[$scope.items]);
+    };
+
+
+// $scope.items = [{
+//         name: "John Smith",
+//         email: "j.smith@example.com",
+//         dob: "1985-10-10"
+//     }, {
+//         name: "Jane Smith",
+//         email: "jane.smith@example.com",
+//         dob: "1988-12-22"
+//     }, {
+//         name: "Jan Smith",
+//         email: "jan.smith@example.com",
+//         dob: "2010-01-02"
+//     }, {
+//         name: "Jake Smith",
+//         email: "jake.smith@exmaple.com",
+//         dob: "2009-03-21"
+//     }, {
+//         name: "Josh Smith",
+//         email: "josh@example.com",
+//         dob: "2011-12-12"
+//     }, {
+//         name: "Jessie Smith",
+//         email: "jess@example.com",
+//         dob: "2004-10-12"
+//     }];
 
 
 
 
-// Fazendo este aqui >>>>>>>>>>>>>>>>>  http://ui-grid.info/docs/#/tutorial/201_editable
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ???
+    alasql('select COD_LOCAL, DESC_LOCAL from xlsx(Lista de Locais.xlsx,{headers:true, range:"B1:E10"})',
+           [],function(items){
+           console.log(items);
+           $scope.items = items;
+       });
 
 
 
